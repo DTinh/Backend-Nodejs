820df5532450a53390eb5dd1c11a0f4c95f0ed15
DTinh <duytinh10833@gmail.com>
2024-10-31 08:36:44 +0700

    nodejs

---
+++ src/migrations/migaration-doctor-infor.js
@@ -0,0 +1,60 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('doctor_infor', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            doctorId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            priceId: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            provinceId: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            paymentId: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            addressClinic: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            nameClinic: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            note: {
+                type: Sequelize.STRING
+            },
+            count: {
+                allowNull: false,
+                defaultValue: 0,
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),  // Thiết lập mặc định là thời gian hiện tại
+                onUpdate: Sequelize.literal('CURRENT_TIMESTAMP')
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('doctor_infor');
+    }
+};
